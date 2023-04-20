ef52bd742d5a3957c70b8b3a268fb8fe99c79cc1
gfdsyjkdyhkfsydkh <thatmuffinhd@gmail.com>
2023-04-20 20:05:56 +0200

    made some changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -7,19 +7,4 @@ window.onscroll = function() {
     document.getElementById("navbar").style.top = "-61px";
   }
   prevScrollpos = currentScrollPos;
-} */
-
-document.querySelectorAll(".recomendations").forEach(recomendation => {
-	const items = recomendation.querySelectorAll(".slide");
-	const buttonsHtml = Array.from(items, () => {
-		return '<span class=carousel-button"></span>';
-	});
-
-	recomendation.insertAdjacentHTML("beforeend", `
-		<div class="carousel-nav">
-			${ buttonsHtml.join("")}
-		</div>
-	`)
-
-	console.log(buttonsHtml);
-});
+}
