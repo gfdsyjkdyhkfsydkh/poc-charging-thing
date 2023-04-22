b271973795aab256c8a066fe408d9d729afd79c5
gfdsyjkdyhkfsydkh <thatmuffinhd@gmail.com>
2023-04-22 15:31:49 +0200

    just comitting before making huge changes

--- script.js
+++ script.js
@@ -24,16 +24,38 @@ document.querySelectorAll(".slider").forEach(slider => {
 		});
 	});
 });
-var input = document.querySelector('input'); // input element
-input.addEventListener('input', resizeInput); // add event listener
-resizeInput.call(input); 
+
+function whatis (value) {
+  return Object.prototype.toString.call(value)
+    .replace(/^\[object\s+([a-z]+)\]$/i, '$1')
+    .toLowerCase();
+}
+
+$(".calc-button").on("click", sizeOfEVInput)
+$(".grid-button").on("click", sizeOfGridInput)
+$("#charging-vehs").on("input",sizeOfEVInput);
+$("#grid-size").on("input",sizeOfGridInput);
+
+function sizeOfGridInput() {
+	document.getElementById("grid-size").style.width = document.getElementById("grid-size").value.length +  3 + "ch";
+}
+
+function sizeOfEVInput() {
+	document.getElementById("charging-vehs").style.width = document.getElementById("charging-vehs").value.length +  3 + "ch";
+}
+
+/* var inputs = document.querySelector('.calc-input-item');
+// $('.calc-input-item').on('input', resizeInput(this))
+inputs.addEventListener('input', resizeInput); // add event listener
+resizeInput.call(inputs);
 
 function resizeInput() {
   this.style.width = this.value.length +  3 + "ch";
-}
+} */
 
 $("#charging-vehs").on("keyup",function(event){
 	var value = $("#charging-vehs").val();
+	value = value * 150 + " kWh";
 	$("#pwr-output").text(value)
 	console.log(value)
 });
